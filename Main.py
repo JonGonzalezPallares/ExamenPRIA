85480765d573dd7f2d65ec4fdbebc3d76b733a75
Javier.Bilbao <jbilbao92@gmail.com>
2023-09-28 12:58:33 +0200

    Cambios

--- Main.py
+++ Main.py
@@ -1,14 +1,10 @@
-from Agregar.py import Agregar
-from Eliminar.py import Eliminar
-from Leer.py import Leer
-from Vista.py import Vista
+import Agregar
+import Eliminar
+import Leer
+import Vista
 class Main():
     def __init__(self):
         self.agre=Agregar()
         self.eli=Eliminar()
         self.visu=Leer()
         self.vist=Vista()
-    
-    
-    
-    
