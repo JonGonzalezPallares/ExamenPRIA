123472a98c73524e8a49141551b425f08853f546
JonGonzalezPallares <jongonzalezpallares0@gmail.com>
2023-09-28 13:40:18 +0200

    Gitignore actualizado

--- Main.py
+++ Main.py
@@ -1,14 +1,12 @@
-from Agregar.py import Agregar
-from Eliminar.py import Eliminar
-from Leer.py import Leer
-from Vista.py import Vista
+#Imports de las clases
+import Agregar
+import Eliminar
+import Leer
+import Vista
+
 class Main():
     def __init__(self):
         self.agre=Agregar()
         self.eli=Eliminar()
         self.visu=Leer()
         self.vist=Vista()
-    
-    
-    
-    
